1ddfe7084c66d0c7205fd63bd0f7ae43a1a1cc52
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-04-02 11:43:21 +0530

    Added: Create a New Contacts in Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,3 +1,40 @@
 package com.bridgelabz;
+import java.util.Scanner;
 public class AddressBook {
+    Scanner scanner = new Scanner(System.in);
+    Contacts contacts = new Contacts();
+    public void addNewContact() {
+        System.out.println("----------------------------------");
+        System.out.println("Enter the Contact Details :-");
+        System.out.println("Enter the First Name :");
+        contacts.setFirstname(scanner.next());
+        System.out.println("Enter the Last Name :");
+        contacts.setLastname(scanner.next());
+        System.out.println("Enter the Address :");
+        contacts.setAddress(scanner.next());
+        System.out.println("Enter the City :");
+        contacts.setCity(scanner.next());
+        System.out.println("Enter the State :");
+        contacts.setState(scanner.next());
+        System.out.println("Enter the PIN Code :");
+        contacts.setPin(scanner.nextLong());
+        System.out.println("Enter the Phone Number :");
+        contacts.setPhoneNumber(scanner.nextLong());
+        System.out.println("Enter the EMail ID :");
+        contacts.setEmail(scanner.next());
+        System.out.println("Contacts Added Successfully------!!!");
+        System.out.println("-----------------------------------------");
+    }
+    public void displayContact() {
+        System.out.println("--------------------------------------------");
+        System.out.println("Contact Details :-");
+        System.out.println("Name : "+contacts.getFirstname()+" "+contacts.getLastname());
+        System.out.println("Address : "+contacts.getAddress());
+        System.out.println("City : "+contacts.getCity());
+        System.out.println("State : "+contacts.getState());
+        System.out.println("PIN Code : "+contacts.getPin());
+        System.out.println("Phone Number : "+contacts.getPhoneNumber());
+        System.out.println("EMail ID : "+contacts.getEmail());
+        System.out.println("---------------------------------------------");
+    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,5 +2,22 @@ package com.bridgelabz;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("--------Welcome to Address Book Program-------");
+        int choice;
+        AddressBook addressBook = new AddressBook();
+        do {
+            System.out.println("1. Add New Contact\n2. Display Contact\n3. Exit");
+            System.out.println("Enter Choice: ");
+            choice = addressBook.scanner.nextInt();
+            if (choice == 1) {
+                addressBook.addNewContact();
+            } else if (choice == 2) {
+                addressBook.displayContact();
+            } else if (choice == 3) {
+                System.out.println("Thank You for Using Address Book.");
+            } else {
+                System.out.println("Please Select between 1 to 3 only.");
+            }
+        }while( choice != 3 );
     }
 }
+
