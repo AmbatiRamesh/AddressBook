26cb082a8d6165ee9ceabc53d1732792fb742f16
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-04-02 10:48:28 +0530

    Added: Ability to Create a Contacts in Address Book

---
+++ Contacts.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+public class Contacts {
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String city;
+    private String state;
+    private Double pin;
+    private Double phoneNumber;
+    private String email;
+}
