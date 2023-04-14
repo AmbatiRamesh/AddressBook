eb3e741f358d025e68c9fb25ed6ab1264a7ae10c
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-04-14 15:44:46 +0530

    Added: no duplicate entry

--- AddressBook.java
+++ AddressBook.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class AddressBook {
     Scanner scanner = new Scanner(System.in);
     Contacts contacts;
-    ArrayList<Contacts>list = new ArrayList<>();
+    ArrayList<Contacts> list = new ArrayList<>();
     public void addNewContact() {
-        contacts=new Contacts();
+        contacts = new Contacts();
         System.out.println("----------------------------------");
         System.out.println("Enter the Contact Details :-");
         System.out.println("Enter the First Name :");
@@ -28,6 +28,34 @@ public class AddressBook {
         System.out.println("Contacts Added Successfully------!!!");
         System.out.println("-----------------------------------------");
         list.add(contacts);
+        System.out.println("Enter First Name :");
+        String firstName = scanner.next();
+        if (contacts.getFirstname().equalsIgnoreCase(firstName)) {
+            System.out.println("Name already Exist----!!");
+        } else {
+            contacts = new Contacts();
+            System.out.println("----------------------------------");
+            System.out.println("Enter the Contact Details :-");
+            System.out.println("Enter the First Name :");
+            contacts.setFirstname(scanner.next());
+            System.out.println("Enter the Last Name :");
+            contacts.setLastname(scanner.next());
+            System.out.println("Enter the Address :");
+            contacts.setAddress(scanner.next());
+            System.out.println("Enter the City :");
+            contacts.setCity(scanner.next());
+            System.out.println("Enter the State :");
+            contacts.setState(scanner.next());
+            System.out.println("Enter the PIN Code :");
+            contacts.setPin(scanner.next());
+            System.out.println("Enter the Phone Number :");
+            contacts.setPhoneNumber(scanner.next());
+            System.out.println("Enter the EMail ID :");
+            contacts.setEmail(scanner.next());
+            System.out.println("Contacts Added Successfully------!!!");
+            System.out.println("-----------------------------------------");
+            list.add(contacts);
+        }
     }
     public void displayContact() {
         if (contacts.getFirstname() == null) {
@@ -39,7 +67,7 @@ public class AddressBook {
         }
     }
     public void editContact() {
-        contacts=new Contacts();
+        contacts = new Contacts();
         System.out.println("Enter the First Name : ");
         String firstName = scanner.next();
         if (firstName.equalsIgnoreCase(contacts.getFirstname())) {
@@ -74,36 +102,8 @@ public class AddressBook {
         if (firstName.equalsIgnoreCase(contacts.getFirstname())) {
             list.remove(contacts);
             System.out.println("Contact Deleted Successfully-------!!!");
-        }else {
+        } else {
             System.out.println("Not Found");
         }
-    }
-    public void contactOptions(AddressBook addressBook) {
-        int choice;
-        do {
-            System.out.println("----Welcome to Address Book System----");
-            System.out.println("1. Add New Contact\n2. Edit Contact\n3. Delete Contact" + "\n4. Display Contact\n5. Exit ");
-            System.out.println("Enter Choice : ");
-            choice = addressBook.scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    addressBook.addNewContact();
-                    break;
-                case 2:
-                    addressBook.editContact();
-                    break;
-                case 3:
-                    addressBook.deleteContact();
-                    break;
-                case 4:
-                    addressBook.displayContact();
-                    break;
-                case 5:
-                    break;
-                default:
-                    System.out.println("Please Select the Operation between 1 to 5 only.");
-                    break;
-            }
-        } while (choice != 5);
     }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,35 +1,25 @@
 package com.bridgelabz;
+import java.util.Scanner;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("--------Welcome to Address Book Program-------");
+        Scanner scanner = new Scanner(System.in);
         int choice;
+        AddressBook addressBook = new AddressBook();
         do {
-            System.out.println("1. Add New AddressBook \n2. Delete AddressBook" + "\n3. Display AddressBook\n4. Add Contact\n5. Exit");
+            System.out.println(
+                    "1. Add New Contact\n2. Edit Contact\n3. Delete Contact" + "\n4. Display Contact\n5. Exit");
             System.out.println("Enter Choice: ");
-            choice = AddressBookCollection.userInput();
-
+            choice = scanner.nextInt();
             switch (choice) {
-                case 1:
-                    AddressBookCollection.addNewAddressBook();
-                    break;
-                case 2:
-                    AddressBookCollection.deleteAddressBook();
-                    break;
-                case 3:
-                    AddressBookCollection.displayAddressBooks();
-                    break;
-                case 4:
-                    AddressBookCollection.selectAddressBook();     
-                    break;
-                case 5:
-                    System.out.println("Thank You for Using Address Book.");
-                    break;
-                default:
-                    System.out.println("Please Select between 1 to 6 only.");
-                    break;
+                case 1 -> addressBook.addNewContact();
+                case 2 -> addressBook.editContact();
+                case 3 -> addressBook.deleteContact();
+                case 4 -> addressBook.displayContact();
+                case 5 -> System.out.println("Thank You for Using Address Book.");
+                default -> System.out.println("Please Select between 1 to 5 only.");
             }
-        }
-        while( choice !=5 );
+        } while (choice != 5);
     }
 }
 
